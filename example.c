250c41a24fbd543da21aaf57a68763242642225b
takkaw <takkaw@gmail.com>
2026-05-24 20:00:31 +0900

    add open and close to example

--- example.c
+++ example.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 #define SKM_NAME_BUF (100)
+#define SKM_EXAMPLE_PORT (1)
 int main(void)
 {
     int port_count = 0;
@@ -14,4 +15,8 @@ int main(void)
             printf("port %d: %s\n", i, port_name);
         }
     }
+
+    skm_port p = {0};
+    skm_port_open(SKM_EXAMPLE_PORT, &p);
+    skm_port_close(&p);
 }
