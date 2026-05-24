ba25ef58f94773ed32260c4e23d41f01274fcd41
takkaw <takkaw@gmail.com>
2026-05-24 09:28:35 +0900

    add internal function port_find

--- skoshmidi.h
+++ skoshmidi.h
@@ -19,11 +19,13 @@ int32_t skm_port_open(uint32_t port, skm_port* p);
 int32_t skm_port_close(skm_port* p);
 int32_t skm_port_recv(skm_port* p, skm_msg* msg);
 
-int32_t skm_port_count(void)
+static int32_t skm_port_find(snd_seq_t** seq_out, int32_t index, snd_seq_port_info_t* port_info_out)
 {
     snd_seq_t* seq;
-    if (snd_seq_open(&seq, "default", SND_SEQ_OPEN_INPUT, 0) < 0)
+    if (snd_seq_open(&seq, "default", SND_SEQ_OPEN_INPUT, 0) < 0) {
         return -1;
+    }
+    *seq_out = seq;
 
     snd_seq_client_info_t* client_info;
     snd_seq_client_info_alloca(&client_info);
@@ -43,11 +45,26 @@ int32_t skm_port_count(void)
         while (snd_seq_query_next_port(seq, port_info) == 0) {
             unsigned int cap = snd_seq_port_info_get_capability(port_info);
             if ((cap & req) == req) {
+                if (count == index) {
+                    if (port_info_out) {
+                        snd_seq_port_info_copy(port_info_out, port_info);
+                    }
+                    return count;
+                }
                 count++;
             }
         }
     }
 
-    snd_seq_close(seq);
+    return count;
+}
+
+int32_t skm_port_count(void)
+{
+    snd_seq_t* seq = NULL;
+    int32_t count = skm_port_find(&seq, -1, NULL);
+    if (seq) {
+        snd_seq_close(seq);
+    }
     return count;
 }
