96383f2df2631a167cb1e005669581baae50b7f8
takkaw <takkaw@gmail.com>
2026-06-17 08:25:33 +0900

    change comment about CoreMIDI

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # `skosh_midi.h`
 A single-header, C11 MIDI 1.0 I/O library licensed under MIT.
 
-- Skosh Supported Platform: ALSA sequencer (Linux).
+- Skosh Supported Platform: ALSA sequencer (Linux), CoreMIDI (macOS).
 - Skosh Features: No MIDI2.0, SysEx, virtual ports, timestamps, scheduled sends, callbacks, hotplug.
 - Skosh MIDI messages: up to 3 bytes.
 - Skosh Usage:
@@ -9,7 +9,9 @@ A single-header, C11 MIDI 1.0 I/O library licensed under MIT.
   #define SKOSH_MIDI_IMPLEMENTATION
   #include "skosh_midi.h"
 ```
-- Skosh Dependencies: -lasound (Linux).
+- Skosh Dependencies:
+  - Linux: -lasound
+  - macOS: -framework CoreFoundation -framework CoreMIDI
 - Skosh MIDI port API: count, name, open, close, recv, send. Returns: >=0 is OK, <0 is Error.
 - Skosh Examples: See the `example` directory.
 
--- skosh_midi.h
+++ skosh_midi.h
@@ -2,15 +2,17 @@
 # `skosh_midi.h`
 A single-header, C11 MIDI 1.0 I/O library licensed under MIT.
 
-- Skosh Supported Platform: ALSA sequencer (Linux).
-- Skosh Features: No MIDI2.0, SysEx, timestamps, scheduled sends, callbacks, hotplug, virtual ports.
+- Skosh Supported Platform: ALSA sequencer (Linux), CoreMIDI (macOS).
+- Skosh Features: No MIDI2.0, SysEx, virtual ports, timestamps, scheduled sends, callbacks, hotplug.
 - Skosh MIDI messages: up to 3 bytes.
 - Skosh Usage:
 ```c
   #define SKOSH_MIDI_IMPLEMENTATION
   #include "skosh_midi.h"
 ```
-- Skosh Dependencies: -lasound (Linux).
+- Skosh Dependencies:
+  - Linux: -lasound
+  - macOS: -framework CoreFoundation -framework CoreMIDI
 - Skosh MIDI port API: count, name, open, close, recv, send. Returns: >=0 is OK, <0 is Error.
 - Skosh Examples: See the `example` directory.
 
