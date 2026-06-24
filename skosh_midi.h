48fcfe1ea4c5a287f7e232b150dd7cef42bb1e8c
takkaw <takkaw@gmail.com>
2026-06-25 06:12:09 +0900

    change README for Windows

--- skosh_midi.h
+++ skosh_midi.h
@@ -1,8 +1,8 @@
 /*
 # `skosh_midi.h`
-A single-header, C11 MIDI 1.0 I/O library.
+A single-header, MIDI 1.0 I/O library.
 
-- Skosh Supported Platform: ALSA sequencer (Linux), CoreMIDI (macOS).
+- Skosh Supported Platform: ALSA sequencer (Linux), CoreMIDI (macOS), WinMM (Windows).
 - Skosh Features: No MIDI2.0, SysEx, virtual ports, timestamps, scheduled sends, callbacks, hotplug.
 - Skosh MIDI messages: up to 3 bytes.
 - Skosh Usage:
@@ -13,7 +13,8 @@ A single-header, C11 MIDI 1.0 I/O library.
 - Skosh Dependencies:
   - Linux: -lasound
   - macOS: -framework CoreFoundation -framework CoreMIDI
-  - Windows(MSYS2/MinGW): -winmm
+  - Windows(MSYS2/MinGW): -lwinmm
+  - Windows(MSVC): winmm
 - Skosh MIDI port API: count, name, open, close, recv, send. Returns: >=0 is OK, <0 is Error.
 - Skosh Examples: See the `example` directory.
 
