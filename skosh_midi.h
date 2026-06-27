828899c3d88f1d36fc2d3ec0d1c74971dfd48dd1
takkaw <takkaw@gmail.com>
2026-06-27 10:52:53 +0900

    fix README

--- skosh_midi.h
+++ skosh_midi.h
@@ -1,25 +1,23 @@
 /*
-# `skosh_midi.h`
-A single-header, MIDI 1.0 I/O library.
+# skosh_midi.h
+A cross-platform single-header MIDI 1.0 I/O library in <500 lines.
 
-- Skosh Supported Platform: ALSA sequencer (Linux), CoreMIDI (macOS), WinMM (Windows).
-- Skosh Features: No MIDI2.0, SysEx, virtual ports, timestamps, scheduled sends, callbacks, hotplug.
-- Skosh MIDI messages: up to 3 bytes.
-- Skosh Usage:
+- Supported platforms: ALSA sequencer (Linux), CoreMIDI (macOS), WinMM (Windows).
+- MIDI messages: up to 3 bytes.
+- Usage:
 ```c
   #define SKOSH_MIDI_IMPLEMENTATION
   #include "skosh_midi.h"
 ```
-- Skosh Dependencies:
+- Dependencies:
   - Linux: -lasound
   - macOS: -framework CoreFoundation -framework CoreMIDI
-  - Windows(MSYS2/MinGW): -lwinmm
-  - Windows(MSVC): winmm
-- Skosh MIDI port API: count, name, open, close, recv, send. Returns: >=0 is OK, <0 is Error.
-- Skosh Ring Buffer API: push, pop. Returns: 0 is OK, -1 is Error.
-- Skosh Examples: See the `example` directory.
-
-**Experimental:** The API may change without notice.
+  - Windows (MSYS2/MinGW): -lwinmm
+  - Windows (MSVC): winmm
+- MIDI port API: count, name, open, close, recv, send. Returns: >=0 OK, <0 error.
+- Ring buffer API: push, pop. Returns: 0 OK, -1 error.
+- Limitations: No MIDI 2.0, SysEx, virtual ports, timestamps, scheduled sends, callbacks, hotplug.
+- Examples: See the `example` directory.
 */
 
 #ifndef SKOSH_MIDI_H
